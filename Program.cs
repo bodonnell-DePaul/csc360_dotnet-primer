d4636c34ef10a8dd72016f8857315742b05c5c87
Brian O'Donnell <brian.m.odonnell@gmail.com>
2024-04-25 02:07:43 +0000

    new webapi project

---
+++ Program.cs
@@ -0,0 +1,74 @@
+using System.Text.Json;
+using dotnet_primer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options => 
+{
+    options.AddPolicy("AllowAllOrigins",
+    builder =>
+    {
+        builder.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAllOrigins");
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+
+app.MapGet("/recipes", () => {
+    return "Hello World!";
+}).WithName("HelloWorld").WithOpenApi();
+
+app.MapGet("/recipeTitles", () => {
+    var options = new JsonSerializerOptions
+    {
+        PropertyNameCaseInsensitive = true
+    };
+    string jsonString = File.ReadAllText("titleData.json");
+    RecipeTitle[]? titles = JsonSerializer.Deserialize<RecipeTitle[]>(jsonString, options);
+    return titles;
+
+}).WithName("GetRecipeTitles").WithOpenApi();
+
+
+app.Run();
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ RecipeTitle.cs
@@ -0,0 +1,18 @@
+﻿namespace dotnet_primer;
+
+public class RecipeTitle
+{
+    public string Title { get; set;}
+    public string? Description { get; set;}
+    public RecipeReview? Review { get; set;}   
+    public string? ImageUrl {get; set;}
+
+    public RecipeTitle(){
+        this.Title = "My Recipe";
+    }
+}
+
+public class RecipeReview{
+    public float Rating { get; set;}
+    public int Reviews { get; set;}
+}
